49085c7843abc8836eddfe379594415030bd9772
Eric.Hsieh <juneyhsieh@gmail.com>
2023-07-24 15:44:39 +0800

    Add mongodb tls setup.

--- keyRegister.sh
+++ keyRegister.sh
@@ -1,2 +1,3 @@
-keytool -import -noprompt -trustcacerts -alias oracleords-service.oracle23c.svc.cluster.local -storepass changeit -keypass changeit -file $KEY_FILE_PATH -keystore /usr/local/openjdk-11/lib/security/cacerts
+keytool -import -noprompt -trustcacerts -alias $ORDS_DNS -storepass changeit -keypass changeit -file $ORDS_KEY_FILE_PATH -keystore /usr/local/openjdk-11/lib/security/cacerts
+keytool -import -noprompt -trustcacerts -alias $MONGODB_DNS -storepass changeit -keypass changeit -file $MONGODB_KEY_FILE_PATH -keystore /usr/local/openjdk-11/lib/security/cacerts
 java -jar /opt/api/student-api.jar
